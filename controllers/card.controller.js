8bab68bb06c2defafa688861298bebf8a00819b0
akash-bt21cse024 <146684897+akash-bt21cse024@users.noreply.github.com>
2024-06-05 11:18:08 +0000

    card all delete

--- controllers/card.controller.js
+++ controllers/card.controller.js
@@ -1,20 +1,21 @@
-const cards=require('../model/card.model');
+const cards = require("../model/card.model");
 
 const postcard = async (req, res) => {
   try {
     const { product, userid: userId } = req.body;
 
     const flag = await cards.findOne({ id: userId });
-    
+
     if (!flag) {
       const newcard = new cards({ id: userId, wishlist: [product] });
       await newcard.save();
       res.status(201).json({ status: "success", messge: "make new card" });
     } else {
       let array = flag.card;
-      const tempflag= array.some(item => item._id === product._id);
-      if(!tempflag){
-      array = [...array, product];}
+      const tempflag = array.some((item) => item._id === product._id);
+      if (!tempflag) {
+        array = [...array, product];
+      }
       await cards.updateOne({ id: userId }, { $set: { card: array } });
       res
         .status(201)
@@ -31,29 +32,25 @@ const postcard = async (req, res) => {
 
 // DELETE: Remove a product from the wishlist
 const deletcard = async (req, res) => {
-const temp=req.params.userid;
-  const temp1=temp.split("+");
-
-  const userId=temp1[0];
-  const productId=temp1[1];
+  const temp = req.params.userid;
+  const temp1 = temp.split("+");
+  
+  const userId = temp1[0];
+  const productId = temp1[1];
   try {
     const flag = await cards.findOne({ id: userId });
 
     if (flag) {
       const arry = flag.card;
-      if(productId==='###')
-      {await cards.updateOne(
-        { id: userId },
-        { $set: { card: []} },
-      );}
-      else
-     { let newarray = arry.filter((item) => item._id !== productId);
-      console.log(newarray);
+      if (productId === "99999") {
+        
+        await cards.updateOne({ id: userId }, { $set: { card: [] } });
+      } else {
+        let newarray = arry.filter((item) => item._id !== productId);
+        console.log(newarray);
 
-      await cards.updateOne(
-        { id: userId },
-        { $set: { card: newarray } },
-      );}
+        await cards.updateOne({ id: userId }, { $set: { card: newarray } });
+      }
       res.status(200).json({
         status: "success",
         message: "product removed from card",
@@ -75,8 +72,7 @@ const temp=req.params.userid;
 // GET: Retrieve the wishlist for a user
 const getcard = async (req, res) => {
   try {
-    const userId=req.params.userid;
-
+    const userId = req.params.userid;
 
     const flag = await cards.findOne({ id: userId });
 
@@ -98,4 +94,4 @@ const getcard = async (req, res) => {
   }
 };
 
-module.exports = {postcard,deletcard,getcard}
+module.exports = { postcard, deletcard, getcard };
